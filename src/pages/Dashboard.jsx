dd59416791b18e04daf8aeeb265536d0d6840b2d
pk-blip <kaleprathmesh440@gmail.com>
2025-11-29 00:25:20 +0530

    final commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet , Link } from 'react-router-dom'
-import Navbar from "./Components/Navbar"
+import Navbar from "../Components/Navbar"
 
 const Dashboard = () => {
   return (
